3266f64cc1bd54187d95578f235bc7d8bc08926f
lilei6809 <lilei6809@gmail.com>
2026-02-28 17:44:37 +0800

    Generating commit message...

--- Chapter05/05-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ Chapter05/05-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -4,8 +4,21 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.With;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
 
+import java.time.Instant;
+
+@With
+@Builder
 public record Book (
+
+        @Id
+        Long id,
 
         @NotBlank(message = "The book ISBN must be defined.")
 		@Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
@@ -19,6 +32,19 @@ public record Book (
 
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.")
-        Double price
+        Double price,
 
-){}
+        @CreatedDate
+        Instant createDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
+
+        @Version
+        Integer version
+
+){
+        public static Book of (String isbn, String title, String author, Double price){
+                return new Book(null, isbn, title, author, price, null, null, 0);
+        }
+}
