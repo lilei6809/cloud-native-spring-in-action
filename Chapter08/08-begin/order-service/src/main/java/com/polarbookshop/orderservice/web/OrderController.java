eafbf55e7963e8cbbb9663d94462704c56824218
lilei6809 <lilei6809@gmail.com>
2026-03-05 22:49:50 +0800

    wsl 最后一次 commit

--- Chapter08/08-begin/order-service/src/main/java/com/polarbookshop/orderservice/web/OrderController.java
+++ Chapter08/08-begin/order-service/src/main/java/com/polarbookshop/orderservice/web/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("orders")
 @Slf4j
 public class OrderController {
     private OrderService orderService;
