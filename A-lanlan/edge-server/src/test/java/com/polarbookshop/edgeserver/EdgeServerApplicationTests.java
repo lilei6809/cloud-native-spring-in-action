74dea0a7e4f877c171907698b150f5da87c2ca3c
lilei6809 <lilei6809@gmail.com>
2026-03-07 13:40:47 +0800

    edge service

---
+++ A-lanlan/edge-server/src/test/java/com/polarbookshop/edgeserver/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
