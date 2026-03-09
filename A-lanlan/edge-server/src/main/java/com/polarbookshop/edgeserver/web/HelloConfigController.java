134afc99184743ae056a2e11765f72412a07b8e6
lilei6809 <lilei6809@gmail.com>
2026-03-09 19:20:22 +0800

    edge service:  trace 配置: 当前的方案是将 opentelemetry 一起注入 image. 使用 jib 控制 java 启动参数

--- A-lanlan/edge-server/src/main/java/com/polarbookshop/edgeserver/web/HelloConfigController.java
+++ A-lanlan/edge-server/src/main/java/com/polarbookshop/edgeserver/web/HelloConfigController.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.edgeserver.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@Slf4j
 public class HelloConfigController {
 
     @Value("${polar.greeting}")
@@ -14,6 +16,7 @@ public class HelloConfigController {
 
     @GetMapping("/")
     public String hello() {
+        log.info("hello");
         return greeting;
     }
 }
